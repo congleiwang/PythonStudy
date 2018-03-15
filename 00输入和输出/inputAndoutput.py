46ffc31b7bf8dae70cbba2b6b952656292dab23c
congl <congleiwang@qq.com>
2018-03-15 16:08:47 +0800

    python学习项目

---
+++ 00输入和输出/inputAndoutput.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 18-3-15 下午3:50
+# @Author  : congl
+# @Site    : 
+# @File    : inputAndoutput.py
+# @Software: PyCharm
+
+# 遇到逗号会输出空格
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+print('100+300=', 100 + 300)
+
+name = input('please enter your name: ')
+
+print(name)
---
+++ 01基础/1.1数据类型.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-3-15 下午3:46
+# @Author  : congl
+# @Site    : 
+# @File    : 1.1数据类型.py
+# @Software: PyCharm
